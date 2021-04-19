7febddf422b93a0d5980669eb96d65897d04f29c
TheTechCrafter <78140568+TheTechCrafter@users.noreply.github.com>
2021-04-19 11:28:45 +0200

    v1.0.0

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace JavaInstaller
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.installButton = new System.Windows.Forms.Button();
+            this.ask = new System.Windows.Forms.Label();
+            this.line = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // installButton
+            // 
+            this.installButton.Enabled = false;
+            this.installButton.Location = new System.Drawing.Point(100, 85);
+            this.installButton.Name = "installButton";
+            this.installButton.Size = new System.Drawing.Size(180, 23);
+            this.installButton.TabIndex = 0;
+            this.installButton.Text = "Java Installieren";
+            this.installButton.UseVisualStyleBackColor = true;
+            this.installButton.Click += new System.EventHandler(this.installButton_Click);
+            // 
+            // ask
+            // 
+            this.ask.AutoSize = true;
+            this.ask.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.ask.Location = new System.Drawing.Point(3, 9);
+            this.ask.Name = "ask";
+            this.ask.Size = new System.Drawing.Size(380, 34);
+            this.ask.TabIndex = 1;
+            this.ask.Text = "Möchten Sie das Java Runtime Environment installieren?\nDieses wird benötigt um di" +
+    "e Stundenplan App zu benutzen.";
+            // 
+            // line
+            // 
+            this.line.AutoSize = true;
+            this.line.Location = new System.Drawing.Point(-30, 62);
+            this.line.Name = "line";
+            this.line.Size = new System.Drawing.Size(451, 13);
+            this.line.TabIndex = 2;
+            this.line.Text = "__________________________________________________________________________";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 116);
+            this.Controls.Add(this.line);
+            this.Controls.Add(this.ask);
+            this.Controls.Add(this.installButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(400, 155);
+            this.MinimumSize = new System.Drawing.Size(400, 155);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "JRE Installer | v1.0.0 - [JAVA v1.15]";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button installButton;
+        private System.Windows.Forms.Label ask;
+        private System.Windows.Forms.Label line;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace JavaInstaller
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            checkForResources();
+        }
+
+        private void closeProgram()
+        {
+            this.Close();
+        }
+
+        private void installButton_Click(object sender, EventArgs e)
+        {
+            installJava();
+            this.Close();
+        }
+
+
+
+
+        // Check For Resources
+        public void checkForResources()
+        {
+
+            string strOutput = findJavaVersion();
+            strOutput = strOutput.Split('.')[0];
+
+            Console.WriteLine("\n" + strOutput);
+
+            if((strOutput.Contains("15")) || strOutput == null)
+            {
+                this.installButton.Enabled = true;
+            }            
+        }
+
+        // Download
+        private void installJava()
+        {
+            string download = Environment.GetEnvironmentVariable("USERPROFILE") + @"\" + "Downloads";
+            Process.Start("notepad.exe", download + "\\InstallApp\\java_15.txt");
+        }
+
+        private string findJavaVersion()
+        {
+            String output = "";
+
+            try
+            {
+                ProcessStartInfo psi = new ProcessStartInfo();
+                psi.FileName = "java.exe";
+                psi.Arguments = " -version";
+                psi.RedirectStandardError = true;
+                psi.UseShellExecute = false;
+
+                Process pr = Process.Start(psi);
+                output = pr.StandardError.ReadLine().Split(' ')[2].Replace("\"", "");
+
+                return output;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ex is: " + ex.Message);
+            }
+
+            return output;
+        }
+    }
+}
